76b617ff168ae6fd3d294a1895755521c0fef9c0
Henrique Campiotti <henrique260699@hotmail.com>
2018-10-20 09:33:22 -0300

    Archiver.py

--- Archiver.py
+++ Archiver.py
@@ -1,144 +1,117 @@
 import sys 
 import os
 
-def criarArchive(nome):
-	with open(nome, 'wb') as archive:
+def criarArchive(nomeArchive):
+	with open(nomeArchive, 'wb') as archive:
 		print('Create Archiver')
 		archive.close()
 
-def addFile(nome, caminhoArchive):
-	with open(caminhoArchive, 'ab+') as archive:
+def addFile(nome, nomeArchive):
+	with open(nomeArchive, 'ab+') as archive:
 		arqPath = open(nome, 'rb')
 		data = arqPath.read()
 		header = nome+'|'+str(len(data))+'|'
-		#print('>> '+ header + str(data) +':'+str(len(data)+len(header)))
 		archive.write(header.encode())
 		archive.write(data)
 		archive.close()
-	print('Add to Archiver')
+	print('Add to '+nomeArchive)
 
-def extractFile(nome):#new
-	with open('Archiver$', 'rb') as f:
+def extractFile(nome, nomeArchive):
+	with open(nomeArchive, 'rb') as archive:
 		pos = 0
-		f.seek(0, 0)
+		archive.seek(0, 0)
 		while True:
-			data = f.read() 	
+			data = archive.read() 	
 			header = data.split(b'|')	
 			if (header[0].decode() == nome):
-				f.seek(pos, 0)
-				data = f.read()
+				archive.seek(pos, 0)
+				data = archive.read()
 				header = (data).split(b'|')
 				pos += len(header[0].decode()) + len(header[1].decode()) + 2
-				f.seek(pos, 0)
-				data = f.read(int(header[1].decode()))
-				o = open(nome, 'wb')
-				o.write(data)
-				o.close()
-				print('Concluido')
+				archive.seek(pos, 0)
+				data = archive.read(int(header[1].decode()))
+				file = open(nome, 'wb')
+				file.write(data)
+				file.close()
+				print('Done!')
 				break
 			pos += len(header[0].decode()) + len(header[1].decode()) + 2 + int(header[1].decode())
-			f.seek(pos, 0)
-		f.close()
+			archive.seek(pos, 0)
+		archive.close()
 
-def listarArchive(nomeArchive):#implementar dic
-	with open(nomeArchive, 'rb') as f:
+def listarArchive(nomeArchive):
+	with open(nomeArchive, 'rb') as archive:
 		pos = 0
-		f.seek(0, 0)
+		archive.seek(0, 0)
 		while True:
 			try:
-				data = f.read() 	
-				header = data.split(b'|')	
+				data = archive.read() 	
+				header = data.split(b'|')
 				print(header[0].decode() + '\t' + header[1].decode() + ' Bytes')
 				pos += len(header[0]) + len(header[1]) + 2 + int(header[1])
-				f.seek(pos, 0)
+				archive.seek(pos, 0)
 			except:
 				break			 
-		f.close()
+		archive.close()
 
-def deleteFile(nome):
-	with open('Archiver$', 'rb') as f:
+def deleteFile(nome, nomeArchive):
+	with open(nomeArchive, 'rb') as archive:
 		pos = 0
-		f.seek(0, 0)
+		archive.seek(0, 0)
 		antesLixo = b''
 		while True:
-			data = f.read()
+			data = archive.read()
 			header = data.split(b'|')
 			if (header[0].decode() == nome):
 				print('Deleting file...')
-				f.seek(pos, 0)
-				data = f.read()
+				archive.seek(pos, 0)
+				data = archive.read()
 				header = (data).split(b'|')
 				headerSize = len(header[0].decode()) + len(header[1].decode()) + 2
-				f.seek(pos, 0)
+				archive.seek(pos, 0)
 				pos += len(header[0].decode()) + len(header[1].decode()) + 2 + int(header[1].decode())
-				lixo = f.read(int(header[1].decode()) + headerSize)
-				f.seek(pos, 0)
-				depoisLixo = f.read()
-				f.close()
-				with open('Archiver$', 'wb') as f:
-					f.write(antesLixo + depoisLixo)
-					f.close()
+				lixo = archive.read(int(header[1].decode()) + headerSize)
+				archive.seek(pos, 0)
+				depoisLixo = archive.read()
+				archive.close()
+				with open(nomeArchive, 'wb') as archive:
+					archive.write(antesLixo + depoisLixo)
+					archive.close()
+				print('Done!')
 				break
-
-			f.seek(pos, 0)
+			archive.seek(pos, 0)
 			headerSize = len(header[0].decode()) + len(header[1].decode()) + 2
-			antesLixo += f.read(int(header[1].decode()) + headerSize)
-
+			antesLixo += archive.read(int(header[1].decode()) + headerSize)
 			pos += len(header[0].decode()) + len(header[1].decode()) + 2 + int(header[1].decode())
-			f.seek(pos, 0)	 		
-		f.close()
+			archive.seek(pos, 0)	 		
+		archive.close()
 
-def showArchiver():
-	f = open('Archiver$', 'rb')
-	data = f.read()
-	print(data)
-	f.close()
+def defaultManager():
+	_help_ = '''
+create    [-c]    ‹novoarchive.arq› ‹arq1› ‹arq2› ...
+insert    [-i]    ‹archive.arq›     ‹arq›
+list      [-l]    ‹archive.arq›
+extract   [-e]    ‹archive.arq›     ‹arq›
+remove    [-r]    ‹archive.arq›     ‹arq›
+'''
+	try:
+		opr = sys.argv[1]
+		if (opr == '-c'):
+			i = 3
+			while i < len(sys.argv):
+				file = sys.argv[i]
+				print(file)
+				addFile(file, sys.argv[2])
+				i+=1
+		elif (opr == '-i'):
+			addFile(sys.argv[3], sys.argv[2])
+		elif (opr == '-l'):
+			listarArchive(sys.argv[2])
+		elif (opr == '-e'):
+			extractFile(sys.argv[3], sys.argv[2])
+		elif (opr == '-r'):
+			deleteFile(sys.argv[3], sys.argv[2])
+	except:
+		print(_help_)
 
-	
-
-def simpleManager():
-	archive = 'Archiver$'
-	banner = '''
-._____________Archiver_____________.
-[0]\tdumpArchive()
-[1]\tcriarArchive(nome)
-[2]\taddFile(nome, caminhoArchive)
-[3]\textractFile(nome)
-[4]\tlistarArchive(nomeArchive)
-[5]\tdeleteFile(nome)
-[6]\tsair()
-			'''
-	while True:
-		print(banner)
-		try:
-			o = int(input('>> '))
-			if (o == 0):
-				showArchiver()
-				os.system('pause')
-			elif (o == 1):
-				criarArchive(archive)
-				os.system('pause')
-			elif (o == 2):
-				nome = input('File Name: ')
-				addFile(nome, archive)
-				os.system('pause')
-			elif (o == 3):
-				nome = input('File Name to Extract: ')
-				extractFile(nome)
-				os.system('pause')
-			elif (o == 4):
-				listarArchive(archive)
-				os.system('pause')
-			elif (o == 5):
-				nome = input('File Name to Delete: ')
-				deleteFile(nome)
-				os.system('pause')
-			elif (o == 6):
-				break
-		except Exception as erro:
-			print(erro)
-			os.system('pause')
-			pass
-		os.system('cls')
-
-simpleManager()
+defaultManager()
